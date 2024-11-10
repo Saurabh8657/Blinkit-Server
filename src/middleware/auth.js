fe456722e59da71d0b369d10c6d65dbf7ea93ea1
Saurabh Ganguly <saurabh.s@lemonn.co.in>
2024-11-10 17:45:15 +0530

    added fetchuser middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken"
+import { Customer, DeliveryPartner } from "../models"
 
 
 
@@ -14,5 +15,26 @@ export const verifyToken = async (req, res, next) => {
         next()
     } catch (error) {
         return  res.status(403).send({message: "Invalid or expired token"})
+    }
+}
+
+
+export const fetchUser = async (req, res) => {
+    try {
+        const { userId, role } = req.user
+        let user
+        if ( role === "Customer" ) {
+            user = await Customer.findById({ _id: userId })
+        } else if ( role === "DeliveryPartner" ) {
+            user = await DeliveryPartner.findById({ _id: userId })
+        } else {
+            return res.status(400).send({ message: "Invalid role" })
+        }
+        if (!user) {
+            return res.status(403).send({ message: "Invalid user" })
+        }
+        return res.status(200).send({ message: "User fetched successfully", user })
+    } catch (error) {
+        return res.status(400).send({ message: "Invalid user", error })
     }
 }
